43803d6fd91c687bd42c177e88d86157dfef58a7
newspaperhobo <hoboinabox@gmail.com>
2023-03-11 14:19:54 -0500

    formatting code and adding comments to match TA solution

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Inside the App() functions, define an array of objects named "list" 
+const list = [
+//Make sure this array has at least two objects with the following fields: id(number), name (string), location(string), followers(number), following(number)
+{ id: 1, name: "Kira", location: "Boston, MA", followers: 457, following: 997 },
+{ id: 2, name: "Sonia", location: "Worcester, MA", followers: 229, following: 1295 },
+{ id: 3, name: "Heidi", location: "Roslindale, MA", followers: 107, following: 194 },
+]
 
 function App() {
-  const list = [
-  {
-    id: 1,
-    name: "Kira",
-    location: "Boston, MA",
-    followers: 457,
-    following: 997 
-  },
-  {
-    id: 2,
-    name: "Sonia",
-    location: "Worcester, MA",
-    followers: 229,
-    following: 1295 
-  },
-  {
-    id: 3,
-    name: "Heidi",
-    location: "Roslindale, MA",
-    followers: 107,
-    following: 194 
-  },
-]
   return (
     <div className="App">
     <h1>Instagram Account Information</h1>
     <label htmlFor="username" className="usernamelabel">Username:</label>
-    <input type="text" id="usernameinput"></input>
+    <input type="text" id="username" name="username"></input>
     <hr></hr>
-     <ListItem />
+    {/* component instantation */}
+     <UserList />
     </div>
   );
 
-  function ListItem() {
-    return list.map((listItem) => (
-      <ul key={listItem.id}>
-        <li>Name: {listItem.name}</li>
-        <li>Location: {listItem.location}</li>
-        <li>Followers: {listItem.followers}</li>
-        <li>Following: {listItem.following}</li>
-      </ul>
-    ));
-  }
 }
 
+// component definition
+function UserList() {
+  //Inside the return, in the main div element, map through this array
+  return list.map((listItem) => (
+    // We will create a "ul" element for each object in the array with each field being a "li" element
+    <ul key={listItem.id}>
+      <li>Name: {listItem.name}</li>
+      <li>Location: {listItem.location}</li>
+      <li>Followers: {listItem.followers}</li>
+      <li>Following: {listItem.following}</li>
+    </ul>
+  ));
+}
 
-
+//  In the end, you will have bullet points displaying each field (except for id) for each object.
 export default App;
