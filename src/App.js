fc9ab4fff9d49ec054e3a8b2b81801f6c8c2b00d
newspaperhobo <hoboinabox@gmail.com>
2023-03-11 13:37:23 -0500

    adds list component, username input, and csss styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const list = [
+  {
+    id: 1,
+    name: "Kira",
+    location: "Boston, MA",
+    followers: 457,
+    following: 997 
+  },
+  {
+    id: 2,
+    name: "Sonia",
+    location: "Worcester, MA",
+    followers: 229,
+    following: 1295 
+  },
+  {
+    id: 3,
+    name: "Heidi",
+    location: "Roslindale, MA",
+    followers: 107,
+    following: 194 
+  },
+]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Instagram Account Information</h1>
+    <label htmlFor="username" className="usernamelabel">Username:</label>
+    <input type="text" id="usernameinput"></input>
+    <hr></hr>
+     <ListItem />
     </div>
   );
+
+  function ListItem() {
+    return list.map((listItem) => (
+      <ul key={listItem.id}>
+        <li>Name: {listItem.name}</li>
+        <li>Location: {listItem.location}</li>
+        <li>Followers: {listItem.followers}</li>
+        <li>Following: {listItem.following}</li>
+      </ul>
+    ));
+  }
 }
+
+
 
 export default App;
