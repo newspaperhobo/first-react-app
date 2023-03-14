64556730b247e9137e28d45fde5fffd070d869a4
newspaperhobo <hoboinabox@gmail.com>
2023-03-13 21:53:55 -0400

    period 3 classwork- using handler functions and props

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { UserList } from './components/UserList';
 
-// Inside the App() functions, define an array of objects named "list" 
-const list = [
-//Make sure this array has at least two objects with the following fields: id(number), name (string), location(string), followers(number), following(number)
-{ id: 1, name: "Kira", location: "Boston, MA", followers: 457, following: 997 },
-{ id: 2, name: "Sonia", location: "Worcester, MA", followers: 229, following: 1295 },
-{ id: 3, name: "Heidi", location: "Roslindale, MA", followers: 107, following: 194 },
-]
+
 
 function App() {
+  // P3 - Move the “users” array inside the App component.
+  // P1 & P2 - Inside the App() functions, define an array of objects named "list" 
+  const userArray = [
+  // P1 & P2 - Make sure this array has at least two objects with the following fields: id(number), name (string), location(string), followers(number), following(number)
+  { id: 1, name: "Kira", location: "Boston, MA", followers: 457, following: 997 },
+  { id: 2, name: "Sonia", location: "Worcester, MA", followers: 229, following: 1295 },
+  { id: 3, name: "Heidi", location: "Roslindale, MA", followers: 107, following: 194 },
+  ]
+  // P3 - Create a new handler function. For naming convention, start with the word “handle”
+  // P3 - Include a parameter. e.target.value grabs text field input value
+  const handleInputChange = (e) => {
+    // P3 - In the function body, console log the parameter.
+    console.log(e.target.value);
+  }
+
   return (
     <div className="App">
-    <h1>Instagram Account Information</h1>
-    <label htmlFor="name" className="namelabel">Name:</label>
-    <input type="text" id="name" name="name"></input>
-    <hr></hr>
-    {/* component instantation */}
-     <UserList />
+      <h1>Instagram Account Information</h1>
+      <label htmlFor="name" className="namelabel">
+        Name:
+      </label>
+      {/* P3 - Attach this parameter to input’s on change event */}
+      <input type="text" id="name" name="name" onChange={handleInputChange} />
+      <hr></hr>
+      {/* P1 & P2 - component instantation */}
+      {/* P3 - Use props to pass this array into the UserItem component */}
+      <UserList userArray={userArray} />
     </div>
   );
 
 }
 
-// component definition
-function UserList() {
-  //Inside the return, in the main div element, map through this array
-  return list.map((listItem) => (
-    // We will create a "ul" element for each object in the array with each field being a "li" element
-    <ul key={listItem.id}>
-      <li>Name: {listItem.name}</li>
-      <li>Location: {listItem.location}</li>
-      <li>Followers: {listItem.followers}</li>
-      <li>Following: {listItem.following}</li>
-    </ul>
-  ));
-}
 
-//  In the end, you will have bullet points displaying each field (except for id) for each object.
+//  P1 & P2 - In the end, you will have bullet points displaying each field (except for id) for each object.
 export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,14 @@
+// P1 & P2 - component definition
+export function UserList(props) {
+    // P3 - Use props as a parameter in the UserItem component and access the same exact information from the props this time (i.e. name, location, etc.
+    // props is an object to in order to access the array you have to call it
+  return props.userArray.map((userEntry) => (
+    // P1 & P2 - We will create a "ul" element for each object in the array with each field being a "li" element
+    <ul key={userEntry.id}>
+      <li>Name: {userEntry.name}</li>
+      <li>Location: {userEntry.location}</li>
+      <li>Followers: {userEntry.followers}</li>
+      <li>Following: {userEntry.following}</li>
+    </ul>
+  ));
+}
